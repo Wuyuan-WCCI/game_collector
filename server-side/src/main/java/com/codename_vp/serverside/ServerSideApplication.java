ca69e857a0bfb34e5fe16db19c50ede357c04a59
wuyuan-wcci <dranest01@gmail.com>
2023-09-05 19:41:42 -0400

    Backend Developer Environment Setup

---
+++ server-side/src/main/java/com/codename_vp/serverside/ServerSideApplication.java
@@ -0,0 +1,14 @@
+package com.codename_vp.serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideApplication.class, args);
+		System.out.println("Welcome to Game Collector");
+	}
+
+}
