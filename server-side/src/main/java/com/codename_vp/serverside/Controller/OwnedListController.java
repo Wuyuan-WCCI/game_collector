d04f9aa928fee8f5583db47bcc05fabd080e2e13
wuyuan-wcci <dranest01@gmail.com>
2023-09-17 16:35:41 -0400

    Add CrossOrigin to the controllers

--- server-side/src/main/java/com/codename_vp/serverside/Controller/OwnedListController.java
+++ server-side/src/main/java/com/codename_vp/serverside/Controller/OwnedListController.java
@@ -2,6 +2,7 @@ package com.codename_vp.serverside.Controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.codename_vp.serverside.Entity.OwnedList;
 import com.codename_vp.serverside.Service.OwnedListService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class OwnedListController {
 
     @Autowired
