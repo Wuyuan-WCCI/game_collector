d1ecfb8e763454b7423f978a434cb4952ae2de5f
codename-carter <tioncarter@gmail.com>
2023-09-23 16:25:40 -0400

    WCCI 2023

--- client-side/src/components/Footer/Foot.jsx
+++ client-side/src/components/Footer/Foot.jsx
@@ -4,7 +4,7 @@ const Foot = () => {
     return (
  <footer className={style.footer}>
     <div className="wrapper">
-      <small>&copy;2017 <strong>Awesome Company</strong>, All Rights Reserved</small>
+      <small>&copy;2023 <strong>WCCI Bootcamp</strong>, Codename VP</small>
       <nav className="footer-nav">
         <a href="#">Back to Top</a>
         <a href="#">Terms of Use</a>
