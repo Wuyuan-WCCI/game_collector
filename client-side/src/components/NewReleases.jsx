9d363fb1bed48b014d8ae596679befc533f1de7c
wuyuan-wcci <dranest01@gmail.com>
2023-10-14 22:50:16 -0400

    add carousel new released game to home page

--- client-side/src/components/NewReleases.jsx
+++ client-side/src/components/NewReleases.jsx
@@ -1,38 +1,17 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Slider from 'react-slick'; // Import the Slider component
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './NewReleases.css'; 
 
 function NewReleases() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
-  // Define your WishList and OwnedList states
-  const [wishList, setWishList] = useState([]);
-  const [ownedList, setOwnedList] = useState([]);
-
-  // // Function to handle adding a game to the WishList
-  // const handleButtonAddWishList = (game) => {
-  //   // Check if the game is already in the WishList to avoid duplicates
-  //   if (!wishList.some((item) => item.id === game.id)) {
-  //     // Add the game to the WishList
-  //     setWishList([...wishList, game]);
-  //   }
-  // };
-
-  // // Function to handle adding a game to the OwnedList
-  // const handleButtonAddOwnedList = (game) => {
-  //   // Check if the game is already in the OwnedList to avoid duplicates
-  //   if (!ownedList.some((item) => item.id === game.id)) {
-  //     // Add the game to the OwnedList
-  //     setOwnedList([...ownedList, game]);
-  //   }
-  // };
-
-
   useEffect(() => {
-    // Define the URL of your backend API
+   // Define the URL of your backend API
     const apiUrl = 'http://localhost:7098/new_games';
 
     fetch(apiUrl)
@@ -58,69 +37,157 @@ function NewReleases() {
       });
   }, []);
 
+  // Configure settings for the carousel
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5, // Number of games to show at once
+    slidesToScroll: 1,
+    prevArrow: <button>Previous</button>, // Custom previous arrow
+    nextArrow: <button>Next</button>, // Customize the right arrow
+  };
+
   if (loading) {
-    return <p>Loading...</p>; // You can show a loading indicator here
+    return <p>Loading...</p>;
   }
 
   if (error) {
-    return <p>Error: {error.message}</p>; // Handle the error here
+    return <p>Error: {error.message}</p>;
   }
 
   return (
-    <div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      {/* <h1>HOT GAMES</h1> */}
-      <div>
-        {/* ...the rest of your game listing */}
-      </div>
-      <div className="container">
+    <div className="carousel-container">
+      <Slider {...settings}>
         {data.map((game) => (
           <Link to={`/game-detail/${game.id}`} key={game.id}>
-            <div className="box">
-              <div className="box-image">
-                <img src={game.background_image} alt={game.name} />
+          <div >
+          <div style={{display: 'block', padding: '20px'}}>
+              <div className="carousel-image">
+                <img src={game.background_image} alt={game.name} className="carousel-item-img" />
               </div>
-              <div className="box-name">
-                <h2>{game.name}</h2>
+              <div >
+                <h2 className="carousel-name">{game.name}</h2>
               </div>
-              <div className="box-name">
-                <h2>{game.description}</h2>
-              </div>
-              {/* Render the platform information for each game */}
-
-              {/* Rest of your game details */}
-                        {/* <div className="box-button">
-            <h2>{game.id}</h2>
-          </div>
-          <div className="box-release">
-            <p><b>Release Date: </b> {game.released}</p>
-          </div>
-          <div className="box-rating">
-            <p><b>Rating: </b> {game.rating}/5</p>
-          </div> */}
-          {/* <div className="box-button">
-            <div className="button-container">
-              Add button to add the game to the WishList 
-              <button onClick={() => handleButtonAddWishList(game)}>Add to WishList</button>
-               Add button to add the game to the OwnedList 
-              <button onClick={() => handleButtonAddOwnedList(game)}>Add to OwnedList</button>
+              {/* ... Other content ... */}
             </div>
-          </div> */}
-            </div>
+          </div>
+           
           </Link>
         ))}
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      </Slider>
     </div>
   );
-  
 }
 
 export default NewReleases;
+
+
+
+
+// import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+
+// function NewReleases() {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+
+ 
+
+
+//   useEffect(() => {
+//     // Define the URL of your backend API
+//     const apiUrl = 'http://localhost:7098/new_games';
+
+//     fetch(apiUrl)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         if (data.results && Array.isArray(data.results)) {
+//           setData(data.results);
+//           setLoading(false);
+//         } else {
+//           // Handle cases where the response structure is unexpected
+//           setError(new Error('Unexpected API response'));
+//           setLoading(false);
+//         }
+//       })
+//       .catch((error) => {
+//         setError(error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) {
+//     return <p>Loading...</p>; // You can show a loading indicator here
+//   }
+
+//   if (error) {
+//     return <p>Error: {error.message}</p>; // Handle the error here
+//   }
+
+//   return (
+//     <div>
+//       <br></br>
+//       <br></br>
+//       <br></br>
+//       <br></br>
+//       {/* <h1>HOT GAMES</h1> */}
+//       <div>
+//         {/* ...the rest of your game listing */}
+//       </div>
+//       <div className="container">
+//         {data.map((game) => (
+//           <Link to={`/game-detail/${game.id}`} key={game.id}>
+//             <div className="box">
+//               <div className="box-image">
+//                 <img src={game.background_image} alt={game.name} />
+//               </div>
+//               <div className="box-name">
+//                 <h2>{game.name}</h2>
+//               </div>
+//               <div className="box-name">
+//                 <h2>{game.description}</h2>
+//               </div>
+//               {/* Render the platform information for each game */}
+
+//               {/* Rest of your game details */}
+//                         {/* <div className="box-button">
+//             <h2>{game.id}</h2>
+//           </div>
+//           <div className="box-release">
+//             <p><b>Release Date: </b> {game.released}</p>
+//           </div>
+//           <div className="box-rating">
+//             <p><b>Rating: </b> {game.rating}/5</p>
+//           </div> */}
+//           {/* <div className="box-button">
+//             <div className="button-container">
+//               Add button to add the game to the WishList 
+//               <button onClick={() => handleButtonAddWishList(game)}>Add to WishList</button>
+//                Add button to add the game to the OwnedList 
+//               <button onClick={() => handleButtonAddOwnedList(game)}>Add to OwnedList</button>
+//             </div>
+//           </div> */}
+//             </div>
+//           </Link>
+//         ))}
+//       </div>
+//       <br></br>
+//       <br></br>
+//       <br></br>
+//       <br></br>
+//     </div>
+//   );
+  
+// }
+
+// export default NewReleases;
 
